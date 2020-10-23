3f378a4fbc33e4f9c675ad99393bd11d5bdc41bc
Matias Galvan <matiasgalvan92@gmail.com>
2020-10-23 18:42:59 -0300

    Connection y QueryType

--- Config/Config.php
+++ Config/Config.php
@@ -7,6 +7,12 @@
     define("VIEWS_PATH", "Views/");
     define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
     define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+
     define("TMDb_KEY", "api_key=a2c8cc87dc896f37eb6ca3258529f6d5");
+
+    define("DB_HOST", "localhost");
+    define("DB_NAME", "MoviePassDB");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
 
 ?>
